5fd671345780e761a3aafe6857a81e9f022319fa
kim-hyunjin <hyunjin1612@gmail.com>
2020-12-05 13:14:43 +0900

    날씨 API 사용 날씨정보 가져오기

--- App.js
+++ App.js
@@ -2,22 +2,28 @@ import React from 'react';
 import {Alert} from 'react-native';
 import Loading from './Loading';
 import * as Location from 'expo-location';
+import axios from 'axios';
+
+const API_KEY = "44691c4927cf5659e3b6fd5d4cca0c32";
 
 export default class extends React.Component {
   state = {
     isLoading: true
   };
+  getWeather = async (latitude, longitude) => {
+    const {data} = await axios.get(`http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${API_KEY}`);
+    console.log(data);
+  };
   getLocation = async () => {
     try {
       await Location.requestPermissionsAsync();
       const {coords: {latitude, longitude}} = await Location.getCurrentPositionAsync();
+      this.getWeather(latitude, longitude);
       this.setState({isLoading: false});
-      console.log(latitude);
-      console.log(longitude);
     } catch (error) {
       Alert.alert("can't find you", error);
     }
-  }
+  };
   componentDidMount() {
     this.getLocation();
   }
