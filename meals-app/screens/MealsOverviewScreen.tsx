1657142b02faa7fb405c3cb053461257292f1c32
kim-hyunjin <hyunjin1612@gmail.com>
2023-05-10 16:08:41 +0900

    setting navigation options

--- meals-app/screens/MealsOverviewScreen.tsx
+++ meals-app/screens/MealsOverviewScreen.tsx
@@ -1,16 +1,24 @@
 import { View, Text, StyleSheet, FlatList } from 'react-native';
-import { MEALS } from '../data/dummy-data';
+import { CATEGORIES, MEALS } from '../data/dummy-data';
 import { NativeStackScreenProps } from '@react-navigation/native-stack';
 import { StackParamList } from '../App';
 import Meal from '../models/meal';
 import MealItem from '../components/MealItem';
+import { useEffect, useLayoutEffect } from 'react';
 
 const MealsOverviewScreen = ({
   route,
+  navigation,
 }: NativeStackScreenProps<StackParamList, 'MealsOverview'>) => {
   const { categoryId } = route.params;
 
   const displayMeals = MEALS.filter((mealItem) => mealItem.categoryIds.indexOf(categoryId) !== -1);
+
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      title: CATEGORIES.find((category) => category.id === categoryId)?.title,
+    });
+  }, [categoryId, navigation]);
 
   const renderMealItem = (item: Meal) => {
     return (
