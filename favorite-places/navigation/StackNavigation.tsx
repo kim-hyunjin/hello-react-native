31d71383fef5e27d960384fa489c0324ad064338
kim-hyunjin <hyunjin1612@gmail.com>
2023-05-13 21:48:06 +0900

    route to add page

--- favorite-places/navigation/StackNavigation.tsx
+++ favorite-places/navigation/StackNavigation.tsx
@@ -2,6 +2,7 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { RouteName } from './route-name';
 import AllPlaces from '../screens/AllPlaces';
 import AddPlace from '../screens/AddPlace';
+import IconButton from '../components/UI/IconButton';
 
 export type StackParamList = {
   [RouteName.ALL_PLACES]: undefined;
@@ -13,7 +14,24 @@ const Stack = createNativeStackNavigator<StackParamList>();
 export default function StackNavigation() {
   return (
     <Stack.Navigator>
-      <Stack.Screen name={RouteName.ALL_PLACES} component={AllPlaces} />
+      <Stack.Screen
+        name={RouteName.ALL_PLACES}
+        component={AllPlaces}
+        options={({ navigation }) => ({
+          headerRight({ tintColor }) {
+            return (
+              <IconButton
+                icon='add'
+                size={24}
+                color={tintColor!}
+                onPress={() => {
+                  navigation.navigate(RouteName.ADD_PLACE);
+                }}
+              />
+            );
+          },
+        })}
+      />
       <Stack.Screen name={RouteName.ADD_PLACE} component={AddPlace} />
     </Stack.Navigator>
   );
