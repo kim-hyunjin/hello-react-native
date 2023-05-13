dfa4725b79b8baceb3db357904b6c3eafac5c47b
kim-hyunjin <hyunjin1612@gmail.com>
2023-05-13 22:20:01 +0900

    place form

--- favorite-places/components/Places/PlaceForm.tsx
+++ favorite-places/components/Places/PlaceForm.tsx
@@ -1,5 +1,40 @@
-import { View } from 'react-native';
+import { useState } from 'react';
+import { ScrollView, Text, TextInput, View, StyleSheet } from 'react-native';
+import { Colors } from '../../constants/colors';
 
 export default function PlaceForm() {
-  return <View></View>;
+  const [enteredTitle, setEnteredTitle] = useState('');
+
+  const handleChangeTitle = (text: string) => {
+    setEnteredTitle(text);
+  };
+  return (
+    <ScrollView style={styles.form}>
+      <View>
+        <Text style={styles.label}>Title</Text>
+        <TextInput style={styles.input} value={enteredTitle} onChangeText={handleChangeTitle} />
+      </View>
+    </ScrollView>
+  );
 }
+
+const styles = StyleSheet.create({
+  form: {
+    flex: 1,
+    padding: 24
+  },
+  label: {
+    fontWeight: 'bold',
+    marginBottom:4,
+    color: Colors.primary500
+  },
+  input: {
+    marginVertical: 8,
+    paddingHorizontal: 4,
+    paddingVertical: 8,
+    fontSize: 16,
+    borderBottomColor: Colors.primary700,
+    borderBottomWidth: 2,
+    backgroundColor: Colors.primary100
+  }
+})
